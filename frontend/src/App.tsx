865b4c3e766471f66b2acdfd6dcef621e496202d
Bhushan <alhatbhushan@gmail.com>
2025-02-22 23:56:14 +0530

    fetch data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,27 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:3000/hello")
+      .then((response) => response.text())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
+    fetch(
+      `http://localhost:3000/hello/${Math.floor(Math.random() * 10e16)
+        .toString(16)
+        .toUpperCase()}`,
+    )
+      .then((response) => response.text())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
+    fetch("http://localhost:3000/todos")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  });
+
   return (
     <div className="App">
       <h1>Live Preview</h1>
