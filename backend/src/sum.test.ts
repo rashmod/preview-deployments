ce292fed50758c1a94fac2f96bb008a46e479f38
Bhushan <alhatbhushan@gmail.com>
2025-02-26 00:58:51 +0530

    add testing

---
+++ backend/src/sum.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { sum } from "./sum.js";
+
+test("passing test", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("failing test", () => {
+  expect(sum(1, 2)).toBe(4);
+});
