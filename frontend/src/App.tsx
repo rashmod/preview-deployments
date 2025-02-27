3c407501834b5c85c13c95d0dc665c0cda9b5ed5
Bhushan <alhatbhushan@gmail.com>
2025-02-27 12:30:41 +0530

    add input form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,38 @@
 import { useEffect } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+});
+
 function App() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   useEffect(() => {
     fetch(`${API_URL}/hello`)
       .then((response) => response.text())
@@ -17,16 +47,28 @@ function App() {
       .then((response) => response.text())
       .then((data) => console.log(data))
       .catch((error) => console.error(error));
-
-    fetch(`${API_URL}/todos`)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
   });
 
   return (
-    <div className="App">
-      <h1>Live Preview</h1>
+    <div className="w-1/2 mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-2">Live Preview</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex gap-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="title" {...field} className="w-60" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 }
